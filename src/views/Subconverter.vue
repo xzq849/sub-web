288536808b06f0c239e0790ba5d1d63609403d04
boring life <70834971+xzq849@users.noreply.github.com>
2024-04-08 11:54:43 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -210,7 +210,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "http://127.0.0.1:25500/sub?" }],
+        backendOptions: [{ value: "https://sub.jkdct.v6.navy/sub?" }],
         remoteConfig: [
           {
             label: "universal",
